59ed46a97636527dde67a20f97ce9d9f3aa25b80
Aandi1989 <navaliny1989@gmail.com>
2023-12-11 11:16:00 +0300

    fixed error with google autorithation (new project)

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,36 +1,57 @@
-'use client';
+"use client";
 
-import Link from 'next/link'
-import { usePathname } from 'next/navigation';
-import React from 'react'
-import { AiFillBug } from 'react-icons/ai';
-import classNames from 'classnames';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import { AiFillBug } from "react-icons/ai";
+import classNames from "classnames";
+import { useSession } from "next-auth/react";
+import { Box, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
-    const currentPath = usePathname();
+  const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
-    const links = [
-        {label: 'Dashboard', href: '/'},
-        {label: 'Issues', href: '/issues'},
-    ]
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues" },
+  ];
 
   return (
-    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-        <Link href="/"><AiFillBug/></Link>
-        <ul className='flex space-x-6'>
-            {links.map(link => 
-                <Link 
-                    key={link.href} 
-                    className={classNames({
-                        'text-zinc-900' : link.href === currentPath,
-                        'text-zinc-500' : link.href !== currentPath,
-                        'hover:text-zinc-800 transition-colors' : true
-                    })} 
-                    href={link.href}>{link.label}</Link>)}
-        </ul>
+    <nav className="border-b mb-5 px-5 h-14">
+      <Flex>
+        <Flex>
+          <Link href="/">
+            <AiFillBug />
+          </Link>
+          <ul className="flex space-x-6">
+            {links.map((link) => (
+              <li key={link.href}>
+                <Link
+                  className={classNames({
+                    "text-zinc-900": link.href === currentPath,
+                    "text-zinc-500": link.href !== currentPath,
+                    "hover:text-zinc-800 transition-colors": true,
+                  })}
+                  href={link.href}
+                >
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </Flex>
+        <Box>
+        {status === "authenticated" && (
+          <Link href="/api/auth/signout">Log out</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
+      </Box>
+      </Flex>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
-
+export default NavBar;
